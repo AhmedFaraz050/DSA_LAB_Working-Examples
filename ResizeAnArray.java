0d541bbf8390c29fb10535c0bfe0ae2308f43892
AhmedFaraz050 <ahmed.faraz44418@gmail.com>
2025-01-21 22:17:29 +0500

    Add files via upload

---
+++ ResizeAnArray.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dsa_lab01;
+
+/**
+ *
+ * @author danis
+ */
+
+public class ResizeAnArray {
+
+    public static void main(String[] args) {
+        int[] originalarr = {13, 2, 34, 4, 59}; 
+        int newsize = 8;
+        int[] resizearr = new int[newsize]; 
+
+       
+        for (int i = 0; i < originalarr.length; i++) {
+            resizearr[i] = originalarr[i]; 
+        }
+
+        
+        System.out.println("Resized Array:");
+        for (int i = 0; i < resizearr.length; i++) {
+            System.out.print(resizearr[i]+" "); 
+        }
+    }
+}
---
+++ Second_max.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dsa_lab01;
+
+/**
+ *
+ * 
+ */
+public class Second_max {
+    public static void main(String[] args) {
+       int arr[] = {9,3,13,1,2};
+       
+     int secmax=0;
+       int max=arr[0];
+       for(int i=1;i<arr.length;i++) {
+           if(max < arr[i]) {
+               secmax=max;
+               max=arr[i];
+       }
+           else if(max>arr[i] && secmax<arr[i]) {
+               secmax=arr[i];
+               max=max;
+           }    
+           
+       }
+       System.out.println("Second maximum value is:"+secmax);
+       } 
+}
+
+
+
